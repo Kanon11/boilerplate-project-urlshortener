533bfc0c74c4f16561bca9dc5c6f6cc40d9b7813
Kanon11 <kanoncuetcse11@gmail.com>
2023-11-16 17:40:40 +0600

    request got

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
+
+// parse Url-encoded bodies (html forms)
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse json bodies (Api requst)
+app.use(bodyParser.json());
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -17,6 +24,10 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+app.use('/api/shorturl', function (req, res) {
+  let { url } = req.body;
+  res.json({ original_url: url, short_url: url });
 });
 
 app.listen(port, function() {
